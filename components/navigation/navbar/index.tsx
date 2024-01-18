8164bbf3c520c18e0b6e8e0b731ea3fc557845ea
bagusyanuar <bagus.yanuar613@gmail.com>
2024-01-18 22:30:51 +0700

    carousel

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,116 +1,42 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
-import styled from 'styled-components'
-import { device } from '@/components/media'
-import { NavbarWrapper, NavLinkWrapper, NavLink } from '@/components/navigation/navbar/styled-components'
-
-
-
-const NavActionWrapper = styled.div`
-    display: flex;
-    align-items: center;
-
-    @media ${device.laptop} {
-      display: flex;
-      align-items: center;
-      gap: 0.4rem;
-    }
-`
-
-const NavAction = styled.a<{ $hideonsmall: boolean }>`
-    cursor: pointer;
-    text-decoration: none;
-    width: 2rem;
-    height: 2rem;
-    border-radius: 50%;
-    display: ${props => props.$hideonsmall ? 'none' : 'flex'};
-    align-items: center;
-    justify-content: center;
-
-    @media ${device.laptop} {
-      display: flex;
-      align-items: center;
-    }
-
-    &:hover {
-      background-color: rgba(220, 220, 220, 0.3)
-    }
-`
-
-NavAction.defaultProps = {
-  $hideonsmall: true
-}
-
-const NavMenuAction = styled.a`
-    cursor: pointer;
-    text-decoration: none;
-    width: 2rem;
-    height: 2rem;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    &:hover {
-      background-color: rgba(220, 220, 220, 0.3)
-    }
-
-    @media ${device.laptop} {
-        display: none;
-    }
-`
-
-const BackdropLayer = styled.div<{ $active: boolean }>`
-    display: ${(props) => props.$active ? 'block' : 'none'};
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100vh;
-    z-index: 9999;
-    background-color: rgba(0, 0, 0, 0.5);
-`
-
-const NavSide = styled.div<{ $active: boolean }>`
-    position: absolute;
-    top: 0;
-    transform: ${(props) => props.$active ? 'translateX(0%)' : 'translateX(-100%)'};
-    transition: transform 0.3s ease-in-out;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 300px;
-    height: 100vh;
-    background-color: white;
-    padding: 1rem 0.5rem;
-`
+import {
+  NavbarWrapper,
+  NavLinkWrapper,
+  NavLink,
+  NavActionWrapper,
+  NavAction,
+  NavMenu,
+  NavSidebarBackdrop,
+  NavSidebar
+} from '@/components/navigation/navbar/styled-components'
 
 function Index() {
-  const [backdropLayerShow, setBackdropLayerShow] = useState<boolean>(false);
-  const [navSideShow, setNavSideShow] = useState<boolean>(false);
+  const [backdropSidebarShow, setBackdropSidebarShow] = useState<boolean>(false);
+  const [sidebarShow, setSidebarShow] = useState<boolean>(false);
 
-  const handleShowNavSide = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+  const handleShowNavSidebar = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     e.preventDefault()
-    setBackdropLayerShow(!backdropLayerShow)
+    setBackdropSidebarShow(!backdropSidebarShow)
     setTimeout(() => {
-      setNavSideShow(!navSideShow)
+      setSidebarShow(!sidebarShow)
     }, 100);
   }
 
-  const handleCloseNavSide = () => {
-    setBackdropLayerShow(false)
-    setNavSideShow(false)
+  const handleCloseSidebar = () => {
+    setBackdropSidebarShow(false)
+    setSidebarShow(false)
   }
 
 
   return (
     <>
       <NavbarWrapper>
-        <NavMenuAction onClick={handleShowNavSide}>
+        <NavMenu onClick={handleShowNavSidebar}>
           <i className='bx bx-menu'></i>
-        </NavMenuAction>
+        </NavMenu>
         <Image src="/assets/images/brand.png" height={40} width={60} alt='brand-logo' />
-        <NavLinkWrapper className='ga'>
+        <NavLinkWrapper className=''>
           <NavLink href='#'>PRODUK</NavLink>
           <NavLink href='#'>KOMUNITAS</NavLink>
           <NavLink href='#'>PENDAFTARAN MITRA</NavLink>
@@ -128,13 +54,11 @@ function Index() {
           </NavAction>
         </NavActionWrapper>
       </NavbarWrapper>
-      <BackdropLayer $active={backdropLayerShow} onClick={() => {
-        handleCloseNavSide()
-      }}>
-        <NavSide $active={navSideShow}>
+      <NavSidebarBackdrop $active={backdropSidebarShow} onClick={handleCloseSidebar}>
+        <NavSidebar $active={sidebarShow}>
           <Image src="/assets/images/brand.png" height={40} width={80} alt='brand-logo' />
-        </NavSide>
-      </BackdropLayer>
+        </NavSidebar>
+      </NavSidebarBackdrop>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/navigation/navbar'
-import { Carousel } from '@material-tailwind/react'
+import { Carousel, IconButton } from '@material-tailwind/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,9 +9,51 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      {/* <Carousel placeholder={`image`}>
-        <Image src='/assets/sliders/slider-yuspin.png' alt='image-1' width={1200}  height={600}/>
-      </Carousel> */}
+      <section className='px-8'>
+        <Carousel
+          placeholder={`image`}
+          className='rounded-xl h-[550px]'
+          prevArrow={({ handlePrev }) => (
+            <IconButton
+              variant="text"
+              color="red"
+              size="lg"
+              onClick={handlePrev}
+              className="!absolute top-2/4 left-4 -translate-y-2/4"
+              placeholder={undefined}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                />
+              </svg>
+            </IconButton>
+          )}
+        >
+          <Image
+            src='/assets/sliders/slider-yuspin.png'
+            alt='image-1'
+            width={1200}
+            height={600}
+            className='w-full h-full object-contain' />
+          <Image
+            src='/assets/sliders/slider-yuspin.png'
+            alt='image-1'
+            width={1200}
+            height={600}
+            className='w-full h-full object-contain' />
+        </Carousel>
+      </section>
+
     </main>
   )
 }
