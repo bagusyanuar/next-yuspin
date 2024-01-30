1be226cc80bb54290a53c3711d0c46e789ed9a78
bagusyanuar <bagus.yanuar613@gmail.com>
2024-01-30 22:07:20 +0700

    assooo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
+import React, { useState, useEffect } from 'react'
 import { Layout } from '@/templates/layouts'
 
-export default function Home() {
+function Home() {
+
+    let arrIndex = [1, 2, 3, 4, 5]
+
+    const myTimer = () => {
+        arrIndex.splice(0, 1)
+        console.log(arrIndex);
+    }
+    useEffect(() => {
+        // let i = setInterval(myTimer, 10000)
+        // return () => { clearInterval(i) }
+    }, [])
+
+
     return (
         <Layout>
-            
+
         </Layout>
     )
 }
+
+export default Home
